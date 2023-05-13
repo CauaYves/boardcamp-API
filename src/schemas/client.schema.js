7a28b46494f9b3db44cd3754eb2d15e0b409b341
Yves <cauayves5@gmail.com>
2023-05-12 21:50:57 -0300

    hub-fix: regex on cpf validation

--- src/schemas/client.schema.js
+++ src/schemas/client.schema.js
@@ -1,7 +1,7 @@
 import joi from "joi"
 
 export const clientSchema = joi.object({
-    cpf: joi.string().min(11).max(11).required(),
+    cpf: joi.string().pattern(/^[0-9]{11}$/).required(),
     phone: joi.string().min(10).max(11).required(),
     name: joi.string().required(),
     birthday: joi.date().iso().required()
