cddd0b2456349a85bdc9bb8bdbeeb42769484d08
Yves <cauayves5@gmail.com>
2023-05-15 15:36:17 -0300

    fix: update customer

--- src/controllers/client.controller.js
+++ src/controllers/client.controller.js
@@ -53,19 +53,24 @@ export async function postClient(req, res) {
     }
 }
 export async function updateClient(req, res) {
+    const { name, phone, cpf, birthday } = req.body;
+    const id = req.params.id;
+
+    const searchClient = `SELECT * FROM customers WHERE id = $1 LIMIT 1;`;
+    const searchCpf = `SELECT * FROM customers WHERE cpf = $1 AND id <> $2;`;
+    const updateClient = `UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5;`;
+
     try {
-        const { name, phone, cpf, birthday } = req.body
-        const searchCostumer = await db.query(`SELECT * FROM customers WHERE customers.cpf = '${cpf}'`)
-        const nameSearch = searchCostumer.rows[0].name
-        const withoutSpaceName = name.replace(/\s/g, "")
+        const { rows } = await db.query(searchClient, [id]);
+        if (!rows.length) return res.sendStatus(404);
 
-        if (nameSearch !== withoutSpaceName) return res.sendStatus(409)
+        const { rows: other } = await db.query(searchCpf, [cpf, id]);
+        if (other.length > 0) return res.sendStatus(409);
 
-        await db.query(`UPDATE customers SET name = '${name}', phone = '${phone}', birthday = '${birthday}' WHERE cpf = '${cpf}'`);
+        await db.query(updateClient, [name, phone, cpf, new Date(birthday).toISOString().split('T')[0], id]);
 
-        res.sendStatus(200)
-    }
-    catch (error) {
+        return res.sendStatus(200);
+    } catch (error) {
         res.status(500).send(error.message)
     }
 }
